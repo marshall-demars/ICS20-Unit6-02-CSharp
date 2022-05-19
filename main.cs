e222fc138a5968fa95a4b44afd2fd6839448a017
Marshall Demars <99354910+marshall-demars@users.noreply.github.com>
2022-05-19 17:06:40 +0000

    main

--- main.cs
+++ main.cs
@@ -1,7 +1,37 @@
-using System;
+// Created by: Marshall
+// Created on: May 2022
+//
+// This program saves to local storage
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+using System;
+using System.Threading.Tasks;
+using System.IO;
+
+class Program
+{
+    public static async Task Main()
+    {
+        int counter = 0;
+        string repeatQuestion;
+
+        while (true)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Do you want to quit the program?");
+            Console.WriteLine("");
+            Console.WriteLine("Type 'yes' to end the program or type 'no' to add 1 to the counter");
+            repeatQuestion = Convert.ToString(Console.ReadLine());
+            if (repeatQuestion == "yes")
+            {
+                break;
+            }
+            counter++;
+            await File.WriteAllTextAsync("WriteText.txt", counter.ToString());
+            // write to file
+        }
+        // end program
+        Console.WriteLine("");
+        Console.WriteLine("The program has ended!");
+        Console.WriteLine("\nDone.");
+    }
 }
